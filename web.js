9ebfdb87da42638a2a8d866dad852eabad054b36
anurag chandra <anurag.chandra@gmail.com>
2013-07-23 12:25:23 -0700

    Fixed image loading in web.js. Moved images to a directory

--- web.js
+++ web.js
@@ -1,7 +1,13 @@
 var express = require('express');
 
+//var express = require('../..');
+
 var app = express.createServer(express.logger());
 var fs = require('fs');
+
+//app.use(express.static('/home/saasbook/startupeng/bitstarter'));
+
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
   var buffer= fs.readFileSync('index.html');
@@ -9,7 +15,9 @@ app.get('/', function(request, response) {
  // response.send('Hello World 2!');
 });
 
-var port = process.env.PORT || 5000;
+
+
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
